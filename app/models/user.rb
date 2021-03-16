3877a85d0d1e17db3dd2ee4628f3d5a37ddbe5db
kamikenbo <kenta_kamio@icloud.com>
2021-03-16 19:27:08 +0900

    フリマアプリ　修正1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
     validates :birthday
   end
 
-  with_options presence: true, format: { with: /\A[一-龥々]+\z/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: (/\A[ぁ-んァ-ヶ一-龥々ー]+\z/), message: 'Full-width characters' } do
     validates :last_name
     validates :first_name
   end
